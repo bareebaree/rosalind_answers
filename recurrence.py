1806a799992d9f3ec40ada888ff50ce6efbd56a6
James Tate <james_tate@ymail.com>
2025-09-25 12:41:11 +0100

    Add files via upload

---
+++ recurrence.py
@@ -0,0 +1,19 @@
+"""
+Simple answer to recurrence problem on Rosalind.
+"""
+
+# Replace n and k with values from Rosalind.
+n = 30
+k = 3
+
+
+def recursion(n,k):
+
+    f = [1, 1]
+    
+    while len(f) < n:     
+        result = f[-1]+ f[-2]*k
+        f.append(result)
+        
+    return(f[-1]) 
+print(recursion(n,k))
